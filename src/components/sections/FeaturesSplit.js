3f20e66a70ccee3d1dc15d8182c3bfa170d2db5d
Fatima Ezzahra <felemenoun@gmail.com>
2022-06-28 15:54:36 +0100

    Refactor my works section

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 import { SectionSplitProps } from '../../utils/SectionProps';
 import SectionHeader from './partials/SectionHeader';
 import Image from '../elements/Image';
+import ButtonGroup from '../elements/ButtonGroup';
+import Button from '../elements/Button';
 
 const propTypes = {
   ...SectionSplitProps.types
@@ -50,8 +52,8 @@ const FeaturesSplit = ({
   );
 
   const sectionHeader = {
-    title: 'Workflow that just works',
-    paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
+    title: 'My works',
+    paragraph: '_____________________'
   };
 
   return (
@@ -67,14 +69,25 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
+                  React • Redux • JavaScript • CSS • Jest
                   </div>
                 <h3 className="mt-0 mb-12">
-                  Data-driven insights
+                COVID-19 Tracker
                   </h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Covid-19 Tracker is a mobile web application that provides the user the new cases of Covid-19 for each country and also the regions within the country by getting all the data and the statistics from Narrativa.
                   </p>
+                  <br />
+                  <div className="reveal-from-bottom" data-reveal-delay="600">
+                    <ButtonGroup>
+                      <Button tag="a" color="primary" wideMobile href="https://timmychan99.github.io/covid19-tracker/">
+                        Live version
+                      </Button>
+                      <Button tag="a" color="dark" wideMobile href="https://github.com/TimmyChan99/covid19-tracker">
+                        Source Github
+                      </Button>
+                    </ButtonGroup>
+                  </div>
               </div>
               <div className={
                 classNames(
@@ -83,7 +96,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/features-split-image-01.png')}
+                  src={require('./../../assets/images/covid19-2.png')}
                   alt="Features split 01"
                   width={528}
                   height={396} />
@@ -93,14 +106,25 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
+                  Ruby on Rails • React • Redux • JavaScript • Ruby
                   </div>
                 <h3 className="mt-0 mb-12">
-                  Data-driven insights
+                Car rental app
                   </h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                This webapp is allows users to get list of cars, cars details, reserve a car and check their reservations list. If the user is the admin that give them the authorization to delete from or add a new car to the list of car. Users need to sign up or sign in to access the API endponits. They can sign up or sign in using the API endpoints.
+               </p>
+               <br />
+               <div className="reveal-from-bottom" data-reveal-delay="600">
+                  <ButtonGroup>
+                    <Button tag="a" color="primary" wideMobile href="https://car-renta.herokuapp.com/">
+                      Live version
+                    </Button>
+                    <Button tag="a" color="dark" wideMobile href="https://github.com/TimmyChan99/Car-rental-app-back-end">
+                      Source Github
+                    </Button>
+                  </ButtonGroup>
+                </div>
               </div>
               <div className={
                 classNames(
@@ -109,7 +133,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/features-split-image-02.png')}
+                  src={require('./../../assets/images/cars.png')}
                   alt="Features split 02"
                   width={528}
                   height={396} />
@@ -119,14 +143,25 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
-                  </div>
+                  Ruby on Rails • Ruby • Sass • HTML • RSpec
+                </div>
                 <h3 className="mt-0 mb-12">
-                  Data-driven insights
+                  myBudget
                   </h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                myBudget is a mobile web application where you can manage your budget: you have a list of transactions associated with a category so that you can see how much money you spent and on what.
+                </p>
+                <br />
+                  <div className="reveal-from-bottom" data-reveal-delay="600">
+                    <ButtonGroup>
+                      <Button tag="a" color="primary" wideMobile href="https://mybudget-railsapp.herokuapp.com/">
+                        Live version
+                      </Button>
+                      <Button tag="a" color="dark" wideMobile href="https://github.com/TimmyChan99/Budget-app/">
+                        Source Github
+                      </Button>
+                    </ButtonGroup>
+                  </div>
               </div>
               <div className={
                 classNames(
@@ -135,7 +170,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/features-split-image-03.png')}
+                  src={require('./../../assets/images/budget.png')}
                   alt="Features split 03"
                   width={528}
                   height={396} />
